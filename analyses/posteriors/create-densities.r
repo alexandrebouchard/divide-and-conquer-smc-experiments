ae2cf6eaab3b22808ceb16834924fb6437eb2400
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2015-02-16 21:51:14 -0800

    Add samples of one level deeper (just for DC vs STD)

--- analyses/posteriors/create-densities.r
+++ analyses/posteriors/create-densities.r
@@ -20,8 +20,30 @@ data <- dbGetQuery(con, "select sampling_method, (sampling_method || '-' || n_it
                    cast(n_iterations as decimal) > 20 and 
                    variable = 'varSample'")
 
-data <- as.data.frame(lapply(data,function(x) if(is.character(x)|is.factor(x)) gsub("BB","ZZZZ",x) else x))
+data <- as.data.frame(lapply(data,function(x) if(is.character(x)|is.factor(x)) gsub("GIBBS-1000","GIBBS-3M",x) else x))
 
 p <- ggplot(data, aes(x = parameter, colour = sampling_method))  + facet_grid(combination ~ node) + geom_density() + scale_x_continuous(limits=c(0, 2)) + scale_y_continuous(limits=c(0, 10))
 
 ggsave("densities-var.pdf", p, width = 10, height = 12)
+
+con <- dbConnect(drv, "deeper-samples.sqlite")
+
+data <- dbGetQuery(con, "select sampling_method, (sampling_method || '-' || n_iterations) 
+                   as combination, node, _value as parameter  from run where 
+                   variable = 'naturalParam'")
+
+p <- ggplot(data, aes(x = parameter, colour = sampling_method))  + geom_density(adjust=1/2) + facet_grid(combination ~ node) + scale_x_continuous(limits=c(0, 5))
+  
+ggsave("densities-deep.pdf", p, width = 49, height = 2)
+
+
+data <- dbGetQuery(con, "select sampling_method, (sampling_method || '-' || n_iterations) 
+                   as combination, node, _value as parameter  from run where 
+                   variable = 'varSample'")
+
+p <- ggplot(data, aes(x = parameter, colour = sampling_method))  + facet_grid(combination ~ node) + geom_density() + scale_x_continuous(limits=c(0, 2)) + scale_y_continuous(limits=c(0, 10))
+
+
+ggsave("densities-var-deep.pdf", p, width = 49, height = 2)
+
+
--- analyses/posteriors/run.sh
+++ analyses/posteriors/run.sh
@@ -11,5 +11,9 @@
 # wrun-collect -csv samples.csv -select "coalesce(sampling_method,'STAN') as sampling_method,coalesce(dc_n_particles,n_iteration,'200000') as n_iterations,folder_location" -where "(plan = 'GIBBS-new' and init_gibbs_with_std_smc	= 'false' and git_commit_time = '2015-02-12 23:50:23') or (model_use_uniform_variance = 'false' and plan = 'SMC-small' and main_random = '1' and git_commit_time = '2015-02-11 08:20:23') or (stan_data_folder = 'data/NY-stan' and seed = '1') or folder_location = '/Users/bouchard/Documents/experiments/multilevelSMC2/results/all/2015-02-13-10-25-19-YEsnneZV.exec' order by sampling_method,n_iterations" -save
 # cp results/latest/db.sqlite samples.sqlite
 
+# Gather some deeper data too
+# wrun-collect -csv samples.csv -select "sampling_method, dc_n_particles as n_iterations,folder_location" -where "dc_level_cut_off_for_output = '3'" -save
+# cp results/latest/db.sqlite deeper-samples.sqlite
+
 
 Rscript create-densities.r
